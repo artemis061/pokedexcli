5b6633ab2310e2a980d9eb2b0aa100fc02f8d7e2
Andrew Miller <andrew.miller.061@gmail.com>
2026-03-04 12:11:29 -0600

    add repl

--- repl.go
+++ repl.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
@@ -8,4 +11,20 @@ func cleanInput(text string) []string {
 	lower := strings.ToLower(text)
 	result := strings.Fields(lower)
 	return result
+}
+
+func startRepl() {
+	prefix := "Pokedex > "
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Printf("%s", prefix)
+		text := scanner.Text()
+		cleanText := cleanInput(text)
+		if len(cleanText) == 0 {
+			continue
+		}
+
+		command := cleanText[0]
+		fmt.Printf("Your command was: %s\n", command)
+	}
 }
